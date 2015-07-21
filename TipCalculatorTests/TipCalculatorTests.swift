dfb763159bdbd38ee210a261b0bbff7d7d0e3cec
Jeremy McDonald <jeremymcdon@gmail.com>
2015-07-20 21:29:33 -0400

    Help?

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -10,7 +10,7 @@ import UIKit
 import XCTest
 
 class TipCalculatorTests: XCTestCase {
-    
+    // wat??????
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
